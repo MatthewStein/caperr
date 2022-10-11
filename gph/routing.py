068a4b41a240c98d8cb91f3227f64d10ca218490
Matthew Stein <steinviolin@gmail.com>
2022-10-11 03:05:22 -0700

    Try removing ws/team websockets
    PythonAnywhere doesn't seem to support websockets and is failing on adding new team members. Test out fix by removing references?

--- gph/routing.py
+++ gph/routing.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 
 from puzzles.messaging import TeamNotificationsConsumer, HintsConsumer
 
-websocket_urlpatterns = [
-    re_path('^ws/team$', TeamNotificationsConsumer.as_asgi()),
-    re_path('^ws/hints$', HintsConsumer.as_asgi()),
-]
+# websocket_urlpatterns = [
+#     re_path('^ws/team$', TeamNotificationsConsumer.as_asgi()),
+#     re_path('^ws/hints$', HintsConsumer.as_asgi()),
+# ]
